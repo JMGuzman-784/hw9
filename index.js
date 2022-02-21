c0caaf7b8a0c7aa5ae58aae140d00c689358b918
Jose Guzman <Josemguzman784@gmail.com>
2022-02-20 21:38:15 -0600

    fixed files

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 
-const generateREADME = ({ title, description, preview, githublink, linktopage }) => `# ${title}
+const generateREADME = ({ title, description, preview, license, githubUsername, githublink, linktopage, emailAddress }) => `# ${title}
 ## Description
 ${description}
     
 ## Preview
 ![Webpage preview](${preview})
     
+## License
+${license}
+
 ## Link to GithubPages
-* Here is the link to Github : [Github/User](${githublink})
+* Github Username : ${githubUsername}
+* Here is the link to Github : [Github/User]${githublink}
     
 ## Link to Webpages
-* Here is the link to Webpage : [GWebpage/User](${linktopage})
+* Here is the link to Webpage : [GWebpage/User]${linktopage}
+
+##
+* Email Address : ${emailAddress}
 
 `;
 
@@ -30,9 +37,23 @@ inquirer
         message: 'What is your project about?',
     },
     {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Which license did you use?',
+        choices: ['The MIT License', 'The GPL License', 'Apache license', 'N/A'],
+        validate: (value) => {
+            if(value){return true} else {return "Select a License or 'N/A' to continue."}
+        }
+    },
+    {
         type: 'input',
         name: 'preview',
         message: 'Upload a Preview screenshot?',
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your Github Username?',
     },
     {
         type: 'input',
@@ -52,9 +73,14 @@ inquirer
             return valid || "Please enter a valid website";
         },
     },
+    {
+        type: 'input',
+        name: 'emailAddress',
+        message: 'What is your Email Address?',
+    },
 ]).then((answers) => {
       const readMePageContent = generateREADME(answers);
-  
+        console.log(answers);
       fs.writeFile('READMe.md ', readMePageContent, (err) =>
         err ? console.log(err) : console.log('Successfully created ☑️ README.md!')
     );
